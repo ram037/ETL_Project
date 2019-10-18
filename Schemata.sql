e8ddf39bbc36e025c48292bb252deb5df199856f
ram037 <52672321+ram037@users.noreply.github.com>
2019-10-17 21:29:20 -0400

    Update Schemata.sql

--- Schemata.sql
+++ Schemata.sql
@@ -1,25 +1,25 @@
 CREATE TABLE fish (
-Species VARCHAR(225),
+Species CHAR(3),
 scientific_name VARCHAR(225),
 family VARCHAR(225),
 PRIMARY KEY (Species)
 )
 
 CREATE TABLE fish_name (
-Species VARCHAR(225),
+Species CHAR(3),
 name VARCHAR(225),
 FOREIGN KEY (Species) REFERENCES fish(Species)
 )
 
 CREATE TABLE country (
-Country VARCHAR(225),
+Country CHAR(2),
 Description VARCHAR(225),
 PRIMARY KEY (Country)
 )
 
 CREATE TABLE catch (
-Country VARCHAR(225),
-Species VARCHAR(225),
+Country CHAR(2),
+Species CHAR(3),
 Year INT,
 TLW FLOAT,
 FOREIGN KEY (Country) REFERENCES country(Country),
